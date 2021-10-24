e71782adb1dc0832391f80d09b4d96993b607e1f
Shaik-Roshan786 <89920391+Shaik-Roshan786@users.noreply.github.com>
2021-10-24 11:28:06 +0530

    Add files via upload

---
+++ OOPSDemo/BankLibrary/Savings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BankLibrary
+{
+    public class Savings : Bank , IIntrest
+    {
+        public Savings()
+        {
+        }
+        public Savings(string name, double amt) : base(name, amt)
+        {
+        }
+        public double CalculateInterest()
+        {
+            return Balance * .1;
+
+        }
+        public sealed override void Withdraw(double amount)
+        {
+            if ((Balance - amount) > 0)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                throw new BalanceException("Balance not Enough.cannot withdraw");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+public class Current : BankLibrary
+{
+    public override void Withdraw(double amount)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+
+
+
+
