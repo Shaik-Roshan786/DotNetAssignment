5ebeea335447e51d52711fb125d93f3c97a2db68
Shaik-Roshan786 <89920391+Shaik-Roshan786@users.noreply.github.com>
2021-10-24 11:06:53 +0530

    Add files via upload

---
+++ EFCFMastek/EFCFMastek/EFCFContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCFMastek
+{
+  public  class EFCFContext :DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database = EFCFDB;Integrated Security = true;");
+        }
+
+
+    }
+}
